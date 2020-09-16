f39281948d3cd6843f1abbd06750fc5297770b48
jaquiles <jaquiles@minsait.com>
2020-09-16 14:30:26 +0200

    Main

--- src/main/java/com/joseanquiles/sfc/Main.java
+++ src/main/java/com/joseanquiles/sfc/Main.java
@@ -1,9 +1,66 @@
 package com.joseanquiles.sfc;
 
+import java.util.Map;
+
+import com.joseanquiles.sfc.util.ArgsUtil;
+
+
 public class Main {
 
+	private static void printSyntax() {
+		System.out.println(Main.class.getName() + " [-h] -c configFile -l left -r right [-o output]");
+		System.out.println("    -h : show this help");
+		System.out.println("    -c : configuration file (mandatory)");
+		System.out.println("    -s : source directory or file (mandatory)");
+		System.out.println("    -t : target directory or file (mandatory)");
+		System.out.println("    -o : output file (default console)");
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			Map<String, String> argsMap = ArgsUtil.parseArgs(args);
+			
+			if (argsMap.containsKey("h")) {
+				printSyntax();
+			}
+
+			String configFile = null;
+			if (!argsMap.containsKey("c")) {
+				System.err.println("-c argument is mandatory");
+				printSyntax();
+				System.exit(1);
+			} else {
+				configFile = argsMap.get("c");
+			}
+
+			String left = null;
+			if (!argsMap.containsKey("l")) {
+				System.err.println("-l argument is mandatory");
+				printSyntax();
+				System.exit(1);
+			} else {
+				left = argsMap.get("l");
+			}
+
+			String right = null;
+			if (!argsMap.containsKey("r")) {
+				System.err.println("-r argument is mandatory");
+				printSyntax();
+				System.exit(1);
+			} else {
+				right = argsMap.get("r");
+			}
+
+			String output = null;
+			if (argsMap.containsKey("o")) {
+				output = argsMap.get("o");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
--- src/main/java/com/joseanquiles/sfc/engine/SFCEngine.java
+++ src/main/java/com/joseanquiles/sfc/engine/SFCEngine.java
@@ -1,5 +1,86 @@
 package com.joseanquiles.sfc.engine;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.joseanquiles.sfc.configuration.FileComparatorConfiguration;
+import com.joseanquiles.sfc.util.FileUtil;
+
 public class SFCEngine {
 
+	private FileComparatorConfiguration config;
+	
+	public SFCEngine(String configFile, String left, String right, String output) throws Exception {
+		
+		this.config = new FileComparatorConfiguration(configFile);
+
+		List<File> sourceFiles = FileUtil.exploreDir(source, config.getIgnoreFiles(), config.getIgnoreDirs());
+		List<File> revisedFiles = FileUtil.exploreDir(revised, config.getIgnoreFiles(), config.getIgnoreDirs());
+		
+		List<File> deleted = new ArrayList<File>();
+		List<File> created = new ArrayList<File>();
+		List<File> common1 = new ArrayList<File>();
+		List<File> common2 = new ArrayList<File>();
+		
+		// STEP 1: files in original not in revised
+		for (int i = 0; i < sourceFiles.size(); i++) {
+			File f1 = sourceFiles.get(i);
+			File f2 = FileUtil.transformBasePath(source, revised, f1);
+			if (!f2.exists()) {
+				deleted.add(f1);
+			} else {
+				common1.add(f1);
+				common2.add(f2);
+			}
+		}
+		System.out.println("===============================================================");
+		System.out.println("DELETED FILES, total " + deleted.size());
+		for (int i = 0; i < deleted.size(); i++) {
+			System.out.println(deleted.get(i));
+		}
+
+		// STEP 2: files in revised not in source
+		for (int i = 0; i < revisedFiles.size(); i++) {
+			File f2 = revisedFiles.get(i);
+			File f1 = FileUtil.transformBasePath(revised, source, f2);
+			if (!f1.exists()) {
+				created.add(f2);
+			}
+		}
+		System.out.println("===============================================================");
+		System.out.println("CREATED FILES, total " + created.size());
+		for (int i = 0; i < created.size(); i++) {
+			System.out.println(created.get(i));
+		}
+
+		// STEP 3: files in both, source and revised
+		System.out.println("===============================================================");
+		System.out.println("MODIFIED FILES");
+		for (int i = 0; i < common1.size(); i++) {
+			
+			File originalFile = common1.get(i);
+			File revisedFile = common2.get(i);
+			
+			List<SFCFilter> pluginList = config.getPluginsForFile(originalFile);
+							
+			FileComparator fc = new FileComparator(originalFile, revisedFile);
+			List<AbstractDelta<String>> deltas = fc.compare(pluginList);
+			if (deltas.size() > 0) {
+				System.out.println("===================================================");
+				System.out.println("FICHERO:" + common1.get(i));
+				if (!nodiff) {
+					System.out.println("---------------------");
+					for (int j = 0; j < deltas.size(); j++) {
+						System.out.println(DeltaUtil.delta2String(deltas.get(j)));
+					}
+				}
+			}
+		}
+		
+		System.out.println("===============================================================");
+
+		
+	}
+	
 }
