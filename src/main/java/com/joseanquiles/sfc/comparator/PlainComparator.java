0ea9753946d1e949e86a40a258ad30a6bf485971
jaquiles <jaquiles@minsait.com>
2020-09-23 14:37:43 +0200

    Use diff lib in plain text comparator

--- src/main/java/com/joseanquiles/sfc/comparator/PlainComparator.java
+++ src/main/java/com/joseanquiles/sfc/comparator/PlainComparator.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Patch;
+
 public class PlainComparator implements SFCComparator {
 
 	@Override
@@ -14,6 +18,42 @@ public class PlainComparator implements SFCComparator {
 	@Override
 	public List<String> run(List<String> leftLines, List<String> rightLines) {
 		List<String> diffs = new ArrayList<>();
+		try {
+			Patch<String> patch = DiffUtils.diff(leftLines, rightLines);
+			List<AbstractDelta<String>> deltas = patch.getDeltas();
+			for (int i = 0; i < deltas.size(); i++) {
+				
+				diffs.add(deltas.get(i).toString());
+				
+				/*
+				System.out.println(deltas.get(i));
+				AbstractDelta<String> delta = deltas.get(i);
+				AbstractDelta<String> newDelta = null;
+				int sourcePosition = delta.getSource().getPosition() < leftLines.size() ? delta.getSource().getPosition() : leftLines.size()-1;
+				int targetPosition = delta.getTarget().getPosition() < rightLines.size() ? delta.getTarget().getPosition() : rightLines.size()-1;
+				Chunk<String> sourceChunk = new Chunk<String>(leftLines.get(sourcePosition).getLinenumber(), delta.getSource().getLines());
+				Chunk<String> targetChunk = new Chunk<String>(rightLines.get(targetPosition).getLinenumber(), delta.getTarget().getLines());
+				
+				switch (delta.getType()) {
+				case DELETE:
+					newDelta = new DeleteDelta<String>(sourceChunk, targetChunk);
+					break;
+				case CHANGE:
+					newDelta = new ChangeDelta<String>(sourceChunk, targetChunk);
+					break;
+				case INSERT:
+					newDelta = new InsertDelta<String>(sourceChunk, targetChunk);
+					break;
+				default:
+					break;
+				}
+				*/
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return diffs;
+		/*
 		if (leftLines.size() != rightLines.size()) {
 			diffs.add("Los ficheros no tienen el mismo tamaño");
 			return diffs;
@@ -26,6 +66,7 @@ public class PlainComparator implements SFCComparator {
 			}
 		}
 		return diffs;
+		*/
 	}
 
 }
